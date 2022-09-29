e26423208abf1409606574a9354c9c5f9ff49ee2
Ritesh Kumar <nyctonio.dev@gmail.com>
2022-09-29 23:51:18 +0530

    Time: 169 ms (23.57%), Space: 34 MB (43.78%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+		// Method 1: 
+        priority_queue<pair<int,int>>pq;
+        for(auto e:arr){
+            if(k!=0){
+                pq.push({abs(x-e),e});
+                k--;
+            }
+            else{
+                if(abs(x-e)<pq.top().first){
+                    pq.pop();
+                    pq.push({abs(x-e),e});
+                }
+            }
+        }
+        vector<int>ans;
+        while(pq.size()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
